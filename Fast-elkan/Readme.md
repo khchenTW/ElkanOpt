f231e0cc8fe9bd51e970a86b36e8167461e05941
KHCHEN <kuan-hsun.chen@tu-dortmund.de>
2021-02-08 19:54:07 +0100

    Clean up the readme. Test data preprocessing with raw skin_nonskin input

--- Fast-elkan/Readme.md
+++ Fast-elkan/Readme.md
@@ -7,22 +7,19 @@ that's been redirected as input:
 ./driver-standalone <algorithm> <dataset> <number of centers> <output>
 
 You can read the source to find all the possible commands, but here is a summary:
-- algorithm --use the name of algorithm, eg. elkan, FB-elkan, MO-elkan to cluster
-- dataset --use the given path name to a file as the dataset 
-- number of centers --use the given number as initial number of centers
+- algorithm -- the name of algorithm, eg. elkan, FB-elkan, MO-elkan to cluster
+- dataset --the given path to a file as the dataset 
+- number of centers --the given k as initial number of centers
 - output --output the result of clustering eg. <centers> outputs results of all centers. Otherwise, output the results of assigned center of each point 
 For example:
 ```
 make all
-cd bin
-./driver-standalone MO_elkan ../datasets/skin_nonskin_clean.txt 50 centers
+./bin/driver-standalone MO_elkan ../datasets/skin_nonskin_clean.txt 50 centers
 ```
+Please note that yinyang_k-means is incompleted version.
 
-Some notes for the past:
+Some notes for using jupyter notebook:
 - Data preprocess for livsvmdataset
 - data preprocessing.ipythonnotebook
 - jupyter notebook > data prepeocessing 
-- Noted that yinyang_k-means is incompleted version
-   
-
 
--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -4,77 +4,69 @@ from sklearn.datasets import load_svmlight_files
 
 def libsvmPreprocess(pathInput, pathOutput, name):
 
-	# download the raw dataset from libsvm website, e.g., HIGGS.bz2
+	# download the raw dataset from libsvm website, e.g., skin_nonskin, HIGGS.bz2
 
 	# at first load the raw dataset
 	x1 = load_svmlight_file(pathInput+name)
 
 	B = x1[0].toarray()
 	#for toarray
+	line_count = 0
 	with open(pathOutput+name+'.txt', 'w') as myfile:
+		myfile.write(str(len(B))+" "+str(len(B[0]))+"\n") # number data points / number of features
 		for i,line in enumerate(B):
-		#myfile.write("%s\n" % line)
-		
-			line=str(line)+"\n"	
+
+			line=str(line)+"\n"
 			myfile.writelines(line.replace('[','').replace(']',''))
+	myfile.close()
 
-	#save memory
-	f = open(pathOutput+name+'.txt','r')
-	temp = f.read()
-	f.close()
-
-	f = open(pathOutput+name+'_clean.txt', 'w')
-	# KHCHEN
-	# this is not automatic yet
-	f.write("11000000 28\n")
-
-	f.write(temp)
-	f.close()
 
 def insert(originalfile,string):
 	with open(originalfile,'r') as f:
-		with open('/home/qiao/Downloads/datasets/gassensor_clean.txt','w') as f2: 
+		with open('./datasets/gassensor_clean.txt','w') as f2:
 			f2.write(string)
 			f2.write(f.read())
 
 def datasetPreprocess(pathInput, pathOutput, name):
-	
-	# if the raw dataset is downloaded from , e.g., gassensor
-	x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10=load_svmlight_files(("/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch1.dat",
-											   "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch2.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch3.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch4.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch5.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch6.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch7.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch8.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch9.dat",
-											  "/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor/batch10.dat"))
-	B=x1.toarray().tolist()+x2.toarray().tolist()+x3.toarray().tolist()+x4.toarray().tolist()+x5.toarray().tolist()+x6.toarray().tolist()+x7.toarray().tolist()+x8.toarray().tolist()+x9.toarray().tolist()+x10.toarray().tolist()
 
+	# if the raw dataset is downloaded from UCI, e.g., gassensor
+	x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10=load_svmlight_files(("./datasets/gassensor/batch1.dat",
+											   "./datasets/gassensor/batch2.dat",
+											  "./datasets/gassensor/batch3.dat",
+											  "./datasets/gassensor/batch4.dat",
+											  "./datasets/gassensor/batch5.dat",
+											  "./datasets/gassensor/batch6.dat",
+											  "./datasets/gassensor/batch7.dat",
+											  "./datasets/gassensor/batch8.dat",
+											  "./datasets/gassensor/batch9.dat",
+											  "./datasets/gassensor/batch10.dat"), multilabel=True)
+	B=x1.toarray().tolist()+x2.toarray().tolist()+x3.toarray().tolist()+x4.toarray().tolist()+x5.toarray().tolist()+x6.toarray().tolist()+x7.toarray().tolist()+x8.toarray().tolist()+x9.toarray().tolist()+x10.toarray().tolist()
+	# x1,y1,x2,y2=load_svmlight_files(("datasets/gassensor/batch1.dat","datasets/gassensor/batch2.dat"))
+	# B=x1.toarray().tolist()+x2.toarray().tolist()
+	print(test)
 
 	#for tolist
 	import re
 	#B = x1[0].toarray().tolist()
 
-	with open('/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor.txt', 'w') as myfile:
+	with open('./datasets/gassensor.txt', 'w') as myfile:
 		for i,line in enumerate(B):#myfile.write("%s\n" % line)
 			line=str(line)+"\n"
 			myfile.writelines(line.replace('[','').replace(']','').replace(',',' '))
 
 		#os.rename('newfile.txt',originalfile)
-	file=insert('/home/qiao/Desktop/fast-kmeans-master/datasets/gassensor.txt',"14000 128\n")
+	file=insert('./datasets/gassensor.txt',"14000 128\n")
 
 def main():
 	args = sys.argv
-	if len(args) < 4:
-		print ("Usage: python dataPreprocessing.py <flag> <path/of/inputDataset> <path/of/outputDataset> <name>, flag 0 is for LIBSVM")
+	if len(args) < 5:
+		print ("Usage: python dataPreprocessing.py <flag> <path/of/inputDataset> <path/of/outputDataset> <name>, flag 0 for LIBSVM; 1 for UCI repository ")
 		return
 	mode = int(args[1])
 	pathI = str(args[2])
 	pathO = str(args[3])
 	name = str(args[4])
-	
+
 	if mode == 0:
 		libsvmPreprocess(pathI, pathO, name)
 	else:
